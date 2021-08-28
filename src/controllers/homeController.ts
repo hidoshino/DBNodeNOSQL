b0efc2da1e3e0fc9d320791e86c57f5ffa31ed20
hidoshino <83619908+hidoshino@users.noreply.github.com>
2021-08-28 17:19:18 -0300

    Filtrando dados

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -2,9 +2,22 @@ import { Request, Response } from 'express';
 import User from '../models/User';
 
 export const home = async (req: Request, res: Response) => {
-    let users = await User.find({});
-
+    let users = await User.find({
+        interests: "eletrônica"
+    });
     res.json({users});
 }
 
+// $gt -> Greater Than = Maior
+// $gte -> Greater or Equal = Maior ou igual
+// $lt -> Lower Than = Abaixo de
+// $lte -> Lower Than or Equal = Abaixo ou igual
 
+/* 
+    Exemplo:
+    let users = await User.find({
+        age: { $gte: 45} // Maior Igual
+        age: { $gt: 45, $lt: 60 } // Maior que 45 e menor que 60
+    });
+
+*/
